ac63d618ceba848cc143eacb26ebfdd3c7e015a3
vagrant <vagrant@homestead>
2019-02-13 12:49:31 +0000

    基础页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.default')
+@section('title','帮助')
+@section('content')
+
+    <h1>帮助页</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.default')
+@section('title','主页')
+@section('content')
+
+    <h1>主页</h1>
+@stop
